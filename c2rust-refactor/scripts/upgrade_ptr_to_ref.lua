84568aa778ecf13b789c26a9230c4cf1d5e86f47
Mitchel Dickerson <mgdicker@uci.edu>
2019-12-02 18:32:20 -0800

    Fixed bug with early break from len() = 3 case (in ownership.rs).

--- c2rust-refactor/scripts/upgrade_ptr_to_ref.lua
+++ c2rust-refactor/scripts/upgrade_ptr_to_ref.lua
@@ -107,7 +107,7 @@ function ConvConfig.from_marks_and_attrs(marks, attrs)
     local noalias = marks["noalias"]
 
     if not noalias then
-        print("I have reached here.")
+        -- print("I have reached here.")
         return
     end
 
@@ -1654,7 +1654,7 @@ function run_ptr_upgrades(node_id_cfgs)
         refactor:run_command("expand_local_ptr_tys", {})
         -- refactor:run_command("ownership_annotate", {"target"})
         refactor:run_command("ownership_mark_pointers", {})
-        -- refactor:run_command("ownership_pointer_analysis", {"/home/glados/Documents/projects/sample-format/gJSON.txt"})
+        refactor:run_command("ownership_pointer_analysis", {"/home/glados/Documents/projects/sample-format/gJSON.txt"})
         -- refactor:dump_marks()
     end
 
